66b79b0f6ed20957c3207573fbc1748485ace16d
sunmon hossain <khansiam171@gmail.com>
2022-05-08 22:28:10 +0300

    first commit

---
+++ src/summation/Summation.java
@@ -0,0 +1,16 @@
+
+package summation;
+
+public class Summation {
+
+   
+    public static void main(String[] args) {
+        int sum = 0;
+        int n = 50;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+      } 
+      System.out.println("The Sum Of " + n + " is " + sum);
+    }
+    
+}
